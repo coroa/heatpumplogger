003ea6035992024f82a6d33bae6bc9dcd05f2980
Andreas Geiges <account@ageiges.de>
2024-11-23 10:26:35 +0100

    update of app to log data to csv

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
+import os
 import time
-
+import traceback
 from bs4 import BeautifulSoup
 from websockets.sync.client import ClientConnection, connect
+import pandas as pd
+import numpy as np
+#%% config
+non_numeric_var = ['Anlagenstatus/Betriebszustand']
+variable_mapping = { 
+    "Temperaturen/Warmwasser-Ist" : ('Tw Warmasser-Ist', '°C'),
+    "Temperaturen/Warmwasser-Soll" : ('Tw Warmwasser-Soll', '°C'),
+    "Temperaturen/Außentemperatur" : ('Ta Außentemperatur', '°C'),
+    'Temperaturen/Vorlauf'  : ('Th Vorlauf-Ist', '°C'),
+    'Temperaturen/Rücklauf' : ('Th Rücklauf-Ist', '°C'),
+    'Temperaturen/Rückl.-Soll' : ('Th Rücklauf-Soll', '°C'),
+    "Temperaturen/Mischkreis1-Vorlauf" :("Th Mischkreis1-Vorlau-Ist", "°C" ),
+    "Temperaturen/Mischkreis1 VL-Soll" :("Th Mischkreis1-VL-Soll", "°C" ),
+    "Anlagenstatus/Betriebszustand" : ("Betriebszustand", ''),
+    'Anlagenstatus/Heizleistung Ist' : ('Heizleistung Ist', 'kW'),
+    'Anlagenstatus/Abtaubedarf' : ('Abtaubedarf', '%'),
+    
+    "Wärmemenge/Heizung" :  ("Wärmemenge_Heizung","kWh"),
+    "Wärmemenge/Warmwasser" : ("Wärmemenge_Warmwasser", "kWh"),
+    # "Wärmemenge/Gesamt" :  ("kWh"),
+    "Eingesetzte Energie/Heizung" :  ("Eingesetzte Energie_Heizung", "kWh"),
+    "Eingesetzte Energie/Warmwasser" :  ("Eingesetzte Energie_Warmwasser", "kWh"),
+    # "Eingesetzte Energie/Gesamt" =:  ("kWh"),
+    }
 
 
+#%%
 def call(ws: ClientConnection, cmd) -> BeautifulSoup:
     ws.send(cmd)
     return BeautifulSoup(ws.recv(timeout=10), "html.parser")
@@ -12,8 +38,9 @@ def call(ws: ClientConnection, cmd) -> BeautifulSoup:
 def select(ws: ClientConnection, id: str) -> dict[str, tuple]:
     id_map = {}
     info_items = call(ws, f"GET;{id}")
-    section_items = info_items.find(name="content").find_all("item", recursive=False)
-    for section_it in section_items:
+    section_items = info_items.find(name="content").find_all("item", recursive=True)
+    for section_it in section_items[:-1]:
+        # print(section_it)
         section = section_it.find("name").text
         id_map.update(
             {
@@ -21,6 +48,7 @@ def select(ws: ClientConnection, id: str) -> dict[str, tuple]:
                 for it in section_it.find_all("item")
             }
         )
+    
     return id_map
 
 
@@ -37,21 +65,77 @@ def update(ws: ClientConnection, id_map) -> list[tuple]:
             data.append((section, param, value))
     return data
 
-
-def main(ip="192.168.2.254", port=8214):
+def update_loop(ip, port):
     with connect(f"ws://{ip}:{port}/", subprotocols=["Lux_WS"]) as ws:
         menu = call(ws, "LOGIN;999999")
         menu_id = menu.find(string="Informationen").parent.parent.attrs["id"]
-
+    
         id_map = select(ws, menu_id)
-        #
+        i_save=0
+        now = pd.Timestamp.now()
+        filename = f'data/log_{now.strftime("%y-%m-%d")}.csv'
+        if os.path.exists(filename):
+            df = pd.read_csv(filename, index_col=0)
+            
+            old_day = now.strftime('%d')
+            
+            missing_cols = set([x[0] for x in variable_mapping.values()]).difference(df.columns)
+            
+            for col in missing_cols:
+                print(f'Adding column {col}')
+                df[col] = np.nan
+        else:
+            df = pd.DataFrame(columns = [x[0] for x in variable_mapping.values()])
+            old_day = 'startup'
         # update data
+        
+        
+        
         while True:
+            i_save +=1
+            now = pd.Timestamp.now()
+            now_str = now.strftime('%H:%M:%S')
+            day = now.strftime('%d')
+            
             data = update(ws, id_map)
+            
+            if day != old_day:
+                # start new dataframe for new day
+                df = pd.DataFrame(columns = [x[0] for x in variable_mapping.values()])
+                old_day = day
+            
+            df.loc[now_str,:] = 0.
+            
             for section, param, value in data:
-                print(f"{section}/{param} = {value}")
+                var = f"{section}/{param}"
+                # print(f"{var} = {value}")
+                # continue
+                # sdf
+                
+                if var in variable_mapping.keys():
+                    print(f"{var} = {value}")
+                    
+                    if var in non_numeric_var :
+                        df.loc[now_str,variable_mapping[var][0]] = value.replace(variable_mapping[var][1],'')
+            
+                    else:
+                        df.loc[now_str,variable_mapping[var][0]] = float(value.replace(variable_mapping[var][1],''))
+            
+            if i_save ==10:
+                print('saving to disk')
+                i_save = 0
+                filename = f'data/log_{now.strftime("%y-%m-%d")}.csv'
+                df.to_csv(filename)
+            time.sleep(15)
 
-            time.sleep(10)
+def main(ip="192.168.2.254", port=8214):
+    
+    while True: #always restart after error
+    
+        try:
+            update_loop(ip, port)
+        except Exception:
+            print(traceback.format_exc())
 
         # section_items.find_all("item")
 
