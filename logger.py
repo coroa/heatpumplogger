ccb82e2acfe76ba48218f6e619aac14c39850201
Jonas Hoersch <jonas.hoersch@posteo.de>
2024-11-26 12:51:52 +0100

    feat(logger): Use csv.DictWriter to only add new data

--- logger.py
+++ logger.py
@@ -1,43 +1,42 @@
 import os
 import time
 import traceback
+from csv import DictWriter
+
+import pandas as pd
 from bs4 import BeautifulSoup
 from websockets.sync.client import ClientConnection, connect
-import pandas as pd
-import numpy as np
-#%% config
-n_save =1
-log_sleep = 60
 
-non_numeric_var = ['Anlagenstatus/Betriebszustand']
-variable_mapping = { 
-    "Temperaturen/Warmwasser-Ist" : ('Tw Warmasser-Ist', '°C'),
-    "Temperaturen/Warmwasser-Soll" : ('Tw Warmwasser-Soll', '°C'),
-    "Temperaturen/Außentemperatur" : ('Ta Außentemperatur', '°C'),
-    'Temperaturen/Vorlauf'  : ('Th Vorlauf-Ist', '°C'),
-    'Temperaturen/Rücklauf' : ('Th Rücklauf-Ist', '°C'),
-    'Temperaturen/Rückl.-Soll' : ('Th Rücklauf-Soll', '°C'),
-    "Temperaturen/Mischkreis1-Vorlauf" :("Th Mischkreis1-Vorlau-Ist", "°C" ),
-    "Temperaturen/Mischkreis1 VL-Soll" :("Th Mischkreis1-VL-Soll", "°C" ),
-    "Temperaturen/VD-Heizung" : ("Th VD-Heizung" , '°C'),
-    "Temperaturen/Ansaug VD" : ("Temperaturen/Ansaug VD", "°C"),
-    "Wärmequelle-Ein" : ("Wärmequelle-Ein", "°C"),
-    
-    "Anlagenstatus/Betriebszustand" : ("Betriebszustand", ''),
-    'Anlagenstatus/Heizleistung Ist' : ('Heizleistung Ist', 'kW'),
-    'Anlagenstatus/Abtaubedarf' : ('Abtaubedarf', '%'),
-    
-    "Wärmemenge/Heizung" :  ("Wärmemenge_Heizung","kWh"),
-    "Wärmemenge/Warmwasser" : ("Wärmemenge_Warmwasser", "kWh"),
+# %% config
+log_interval = 60
+
+non_numeric_var = frozenset(["Anlagenstatus/Betriebszustand"])
+variable_mapping = {
+    "Temperaturen/Warmwasser-Ist": ("Tw Warmasser-Ist", "°C"),
+    "Temperaturen/Warmwasser-Soll": ("Tw Warmwasser-Soll", "°C"),
+    "Temperaturen/Außentemperatur": ("Ta Außentemperatur", "°C"),
+    "Temperaturen/Vorlauf": ("Th Vorlauf-Ist", "°C"),
+    "Temperaturen/Rücklauf": ("Th Rücklauf-Ist", "°C"),
+    "Temperaturen/Rückl.-Soll": ("Th Rücklauf-Soll", "°C"),
+    "Temperaturen/Mischkreis1-Vorlauf": ("Th Mischkreis1-Vorlau-Ist", "°C"),
+    "Temperaturen/Mischkreis1 VL-Soll": ("Th Mischkreis1-VL-Soll", "°C"),
+    "Temperaturen/VD-Heizung": ("Th VD-Heizung", "°C"),
+    "Temperaturen/Ansaug VD": ("Temperaturen/Ansaug VD", "°C"),
+    "Wärmequelle-Ein": ("Wärmequelle-Ein", "°C"),
+    "Anlagenstatus/Betriebszustand": ("Betriebszustand", ""),
+    "Anlagenstatus/Heizleistung Ist": ("Heizleistung Ist", "kW"),
+    "Anlagenstatus/Abtaubedarf": ("Abtaubedarf", "%"),
+    "Wärmemenge/Heizung": ("Wärmemenge_Heizung", "kWh"),
+    "Wärmemenge/Warmwasser": ("Wärmemenge_Warmwasser", "kWh"),
     # "Wärmemenge/Gesamt" :  ("kWh"),
-    "Eingesetzte Energie/Heizung" :  ("Eingesetzte Energie_Heizung", "kWh"),
-    "Eingesetzte Energie/Warmwasser" :  ("Eingesetzte Energie_Warmwasser", "kWh"),
+    "Eingesetzte Energie/Heizung": ("Eingesetzte Energie_Heizung", "kWh"),
+    "Eingesetzte Energie/Warmwasser": ("Eingesetzte Energie_Warmwasser", "kWh"),
     # "Eingesetzte Energie/Gesamt" =:  ("kWh"),
-    "Durchfluss" : ("Durchfluss", "l/h")
-    }
+    "Durchfluss": ("Durchfluss", "l/h"),
+}
 
 
-#%%
+# %%
 def call(ws: ClientConnection, cmd) -> BeautifulSoup:
     ws.send(cmd)
     return BeautifulSoup(ws.recv(timeout=10), "html.parser")
@@ -56,7 +55,7 @@ def select(ws: ClientConnection, id: str) -> dict[str, tuple]:
                 for it in section_it.find_all("item")
             }
         )
-    
+
     return id_map
 
 
@@ -73,84 +72,82 @@ def update(ws: ClientConnection, id_map) -> list[tuple]:
             data.append((section, param, value))
     return data
 
+
+def update_existing_file(fieldnames: list[str]) -> str:
+    date_str = pd.Timestamp.now().strftime("%y-%m-%d")
+    filename = f"data/log_{date_str}.csv"
+    if not os.path.exists(filename):
+        return "startup"
+
+    tt = time.time()
+    print("Loading from disk and extending with new columns..", end="")
+    df = pd.read_csv(filename, index_col=0)
+
+    # update file if new columns or new order
+    changed_columns = fieldnames[1:] != list(df.columns)
+    if changed_columns:
+        df.reindex(columns=fieldnames[1:]).to_csv(filename)
+    print(f".done in {time.time()-tt:2.2f}s")
+
+    return date_str
+
+
 def update_loop(ip, port):
     with connect(f"ws://{ip}:{port}/", subprotocols=["Lux_WS"]) as ws:
         menu = call(ws, "LOGIN;999999")
         menu_id = menu.find(string="Informationen").parent.parent.attrs["id"]
-    
+
         id_map = select(ws, menu_id)
-        i_save=0
-        now = pd.Timestamp.now()
-        filename = f'data/log_{now.strftime("%y-%m-%d")}.csv'
-        if os.path.exists(filename):
-            tt = time.time()
-            print('Loading from disk..', end='')
-            df = pd.read_csv(filename, index_col=0)
-            print(f'.done in {time.time()-tt:2.2f}s')
-            old_day = now.strftime('%d')
-            
-            missing_cols = set([x[0] for x in variable_mapping.values()]).difference(df.columns)
-            
-            for col in missing_cols:
-                print(f'Adding column {col}')
-                df[col] = np.nan
-        else:
-            df = pd.DataFrame(columns = [x[0] for x in variable_mapping.values()])
-            old_day = 'startup'
+        fieldnames = ["time"] + [field for field, unit in variable_mapping.values()]
+
+        old_date_str = update_existing_file(fieldnames)
+
+        # wait until next full interval before first sync
+        time.sleep(log_interval - (time.localtime().tm_sec % log_interval))
+
         # update data
-        
-        
-        
         while True:
-            tt_start = time.time()
-            i_save +=1
-            now = pd.Timestamp.now()
-            now_str = now.strftime('%H:%M:%S') #ToDo .round('min')
-            day = now.strftime('%d')
-            
+            now = time.time()
+            now_str = time.strftime("%H:%M:%S", time.localtime(now))
+            date_str = time.strftime("%y-%m-%d", time.localtime(now))
+
             data = update(ws, id_map)
-            
-            if day != old_day:
-                # start new dataframe for new day
-                df = pd.DataFrame(columns = [x[0] for x in variable_mapping.values()])
-                old_day = day
-            
-            df.loc[now_str,:] = 0.
-            
-            for section, param, value in data:
-                var = f"{section}/{param}"
-                # print(f"{var} = {value}")
-                # continue
-                # sdf
-                
-                if var in variable_mapping.keys():
-                    # print(f"{var} = {value}")
-                    
-                    if var in non_numeric_var :
-                        df.loc[now_str,variable_mapping[var][0]] = value.replace(variable_mapping[var][1],'')
-            
-                    else:
-                        df.loc[now_str,variable_mapping[var][0]] = float(value.replace(variable_mapping[var][1],''))
-            
-            if i_save ==n_save:
-                tt = time.time()
-                print('saving to disk..', end='')
-                i_save = 0
-                filename = f'data/log_{now.strftime("%y-%m-%d")}.csv'
-                df.to_csv(filename)
-                print(f'.done in {time.time()-tt:2.2f}s')
-                
-            t_calc = time.time()-tt_start
-            time.sleep(log_sleep -t_calc)
+
+            filename = f"data/log_{date_str}.csv"
+            with open(filename, mode="a") as f:
+                writer = DictWriter(f, fieldnames)
+                if date_str != old_date_str:
+                    # new file was started we need to output the header
+                    writer.writeheader()
+                    old_date_str = date_str
+
+                row = dict(time=now_str)
+                for section, param, value in data:
+                    var = f"{section}/{param}"
+
+                    if var in variable_mapping:
+                        field, unit = variable_mapping[var]
+
+                        value = value.replace(unit, "")
+                        if var not in non_numeric_var:
+                            value = float(value)
+                        row[field] = value
+
+                writer.writerow(row)
+
+            print(f".done in {time.time()-now:2.2f}s")
+
+            t_calc = time.time() - now
+            time.sleep(log_interval - t_calc)
+
 
 def main(ip="192.168.2.254", port=8214):
-    os.makedirs('data', exist_ok=True)
-    while True: #always restart after error
-    
+    os.makedirs("data", exist_ok=True)
+    while True:  # always restart after error
         try:
             update_loop(ip, port)
         except Exception:
-            print(f'Warning: Update loop failed at {pd.Timestamp.now()}')
+            print(f"Warning: Update loop failed at {pd.Timestamp.now()}")
             print(traceback.format_exc())
 
 
